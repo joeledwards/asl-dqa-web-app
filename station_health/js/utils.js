97fe4dc8c77b16ffb5fd58b44751fc93ba873183
James Holland <jholland@usgs.gov>
2012-11-26 12:54:01 -0700

    Fixing bug in getQueryString(key) and added author and license info

--- station_health/js/utils.js
+++ station_health/js/utils.js
@@ -1,5 +1,9 @@
-
-//Written 9-17-2012
+/*
+utils.js
+Author: James Holland jholland@usgs.gov
+utils.js contains generic functions that are not tied to a specific project.
+License: Public Domain
+*/
 function pad(input, size) {
     var output = input+"";
     while (output.length<size){
@@ -20,7 +24,7 @@ function getQueryString(key) {
     var queryStrings = window.location.search.substring(1).split('&');
     for(var i = 0; i < queryStrings.length; i++){
         var queryString = queryStrings[i].split('=');
-        if(queryString[0] = key)
+        if(queryString[0] == key)
             return queryString[1];
     }
     return null;
