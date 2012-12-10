5844d5896414041e4075722ee717d522d1d1b716
James Holland <jholland@usgs.gov>
2012-12-10 16:09:08 -0700

    Fixing issue where yearmonth controls would not change even if date range was changed. It now changes the month/year ddls to match the startdate month/year when toggling back to month mode.

--- station_health/js/form.js
+++ station_health/js/form.js
@@ -9,106 +9,108 @@
 function initForm(type){
     var form = $("#frmSelector");
     if (type == "summary"){
-        form.append(
-            "<select id='ddlGroup' class='ui-state-default ui-corner-all'>"+
-                "<option value='0'>All</option>"+
-                "</select>");
-        }
-
-        if (type == "station"){
-            form.append("<a href='summary.html' class='ui-state-default ui-corner-all'>Summary</a>");
-        }
-
-        //Create YearMonth form for easier displaying/hiding
-        var spanYearMonth = $("<span id='spanYearMonth'/>");
-        spanYearMonth.append(
-            "<select id='ddlYear' class='ui-state-default ui-corner-all'>"+
-                "</select>"
-        );
-        spanYearMonth.append("<select id='ddlMonth' class='ui-state-default ui-corner-all'></select>");
-
-        //Create daterange from for easy displaying/hiding
-        var spanDateRange = $("<span id='spanDateRange'/>");
-        spanDateRange.append(
-            "<label for='dpStartDate'>  From</label>"+
-                "<input type='text' id='dpStartDate' name='dpStartDate'/>"
-        );
-        spanDateRange.append(
-            "<label for='dpEndDate'>  To</label>"+
-                "<input type='text' id='dpEndDate' name='dpEndDate'/>"
-        );
-        //Add both spans to the form, then hide daterange
-        form.append(spanYearMonth);
-        form.append(spanDateRange);
-        spanDateRange.hide();
-
-        //Add switch between month and date range
-        form.append(
-            "<button type='button' id='btnToggleDate' class='ui-state-default ui-corner-all'>"+
-                "Toggle Date Range"+
-                "</button>"
-        );
-
-
-        //Add update button
-        form.append(
-            "<button type='button' id='btnUpdate' class='ui-state-default ui-corner-all'>"+
-                "Update"+
-                "</button>"
-        );
-        //Make startDate and endDate datepickers
-        $("#dpStartDate").datepicker({
-                changeMonth: true,
-                changeYear: true,
-                numberOfMonths: 2,
-                dateFormat: "yy-mm-dd",
-                onClose: function(selectedDate){
-                    $("#dpEndDate").datepicker( "option", "minDate", selectedDate);
-                }
-            });
-        $("#dpEndDate").datepicker({
-                changeMonth: true,
-                changeYear: true,
-                numberOfMonths: 2,
-                dateFormat: "yy-mm-dd",
-                onClose: function(selectedDate){
-                    $("#dpStartDate").datepicker( "option", "maxDate", selectedDate);
-                }
-            });
-
-
-        //Bind all form actions now that they controls are created
-        bindFormActions(type);
+        form.append("<select id='ddlGroup' class='ui-state-default ui-corner-all'><option value='0'>All</option></select>");
     }
 
-    function bindFormActions(type){
-        if (type == "summary"){
-            $("#btnUpdate").on('click',function(){
-                    filterGroups(dataGrid);
-                    clearDataTable(dataGrid);
-                    populateGrid(dataGrid);
-                });
-        }
-        else {
-            $("#btnUpdate").on('click',function(){
-                    clearDataTable(dataGrid);
-                    populateGrid(dataGrid);
-                });
-        }
-
-        $("#btnToggleDate").on('click',function(){
-                $("#spanYearMonth").toggle();
-                $("#spanDateRange").toggle();
-            });
-
-        $("#ddlYear").change(function(){
-                year_selected();
-                yearMonthToStartDate();
-                yearMonthToEndDate();
-            });
-        $("#ddlMonth").change(function(){
-                yearMonthToStartDate();
-                yearMonthToEndDate();
-            });
-
+    if (type == "station"){
+        form.append("<a href='summary.html' class='ui-state-default ui-corner-all'>Summary</a>");
     }
+
+    //Create YearMonth form for easier displaying/hiding
+    var spanYearMonth = $("<span id='spanYearMonth'/>");
+    spanYearMonth.append(
+        "<select id='ddlYear' class='ui-state-default ui-corner-all'></select>"
+    );
+    spanYearMonth.append("<select id='ddlMonth' class='ui-state-default ui-corner-all'></select>");
+
+    //Create daterange from for easy displaying/hiding
+    var spanDateRange = $("<span id='spanDateRange'/>");
+    spanDateRange.append(
+        "<label for='dpStartDate'>  From</label>"+
+            "<input type='text' id='dpStartDate' name='dpStartDate'/>"
+    );
+    spanDateRange.append(
+        "<label for='dpEndDate'>  To</label>"+
+            "<input type='text' id='dpEndDate' name='dpEndDate'/>"
+    );
+    //Add both spans to the form, then hide daterange
+    form.append(spanYearMonth);
+    form.append(spanDateRange);
+    spanDateRange.hide();
+
+    //Add switch between month and date range
+    form.append(
+        "<button type='button' id='btnToggleDate' class='ui-state-default ui-corner-all'>"+
+            "Toggle Date Range"+
+            "</button>"
+    );
+
+
+    //Add update button
+    form.append(
+        "<button type='button' id='btnUpdate' class='ui-state-default ui-corner-all'>"+
+            "Update"+
+            "</button>"
+    );
+    //Make startDate and endDate datepickers
+    $("#dpStartDate").datepicker({
+            changeMonth: true,
+            changeYear: true,
+            numberOfMonths: 2,
+            dateFormat: "yy-mm-dd",
+            onClose: function(selectedDate){
+                $("#dpEndDate").datepicker( "option", "minDate", selectedDate);
+            }
+        });
+    $("#dpEndDate").datepicker({
+            changeMonth: true,
+            changeYear: true,
+            numberOfMonths: 2,
+            dateFormat: "yy-mm-dd",
+            onClose: function(selectedDate){
+                $("#dpStartDate").datepicker( "option", "maxDate", selectedDate);
+            }
+        });
+
+
+    //Bind all form actions now that they controls are created
+    bindFormActions(type);
+}
+
+function bindFormActions(type){
+    if (type == "summary"){
+        $("#btnUpdate").on('click',function(){
+                filterGroups(dataGrid);
+                clearDataTable(dataGrid);
+                populateGrid(dataGrid);
+            });
+    }
+    else {
+        $("#btnUpdate").on('click',function(){
+                clearDataTable(dataGrid);
+                populateGrid(dataGrid);
+            });
+    }
+
+    $("#btnToggleDate").on('click',function(){
+            $("#spanYearMonth").toggle();
+            $("#spanDateRange").toggle();
+            if($("#spanYearMonth").is(":visible")){ //If switching back to YearMonthView, take year and month from startDate
+                $("#ddlYear").val(getStartDate().getUTCFullYear());
+                $("#ddlMonth").val(getStartDate().getUTCMonth()); 
+                yearMonthToStartDate(); //push yearmonth back to startend date fields
+                yearMonthToEndDate();
+            }
+        });
+
+    $("#ddlYear").change(function(){
+            year_selected();
+            yearMonthToStartDate();
+            yearMonthToEndDate();
+        });
+    $("#ddlMonth").change(function(){
+            yearMonthToStartDate();
+            yearMonthToEndDate();
+        });
+
+}
