893f07272f582edbeb9c4d9de4c363a16d832531
James Holland <jholland@usgs.gov>
2012-12-21 11:44:27 -0700

    Changed the station link on the summary page. It now does the page load and dynamically pulls the dates. The station page still doesn't check for passed dates.

--- station_health/js/datatable.js
+++ station_health/js/datatable.js
@@ -8,7 +8,14 @@
 function bindDatatableActions(datatable) {
     datatable.find('tbody td').click(function(){
             var id = $(this).attr('id');
-            if(id != undefined && id.charAt(0) == 'd')
-                createDialog(id);
+            if(id != undefined){
+                if(id != undefined && id.charAt(0) == 'd')
+                    createDialog(id);
+                else if(id.charAt(0) == 'l'){
+                    window.location = 'station.html?station='+id.split('_')[1]
+                    +'&sdate='+getStartDate('simple')
+                    +'&edate='+getEndDate('simple');
+                }
+            }
         });
 }
