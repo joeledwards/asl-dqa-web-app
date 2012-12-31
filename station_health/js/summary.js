b1041c4cac0673474dac9334e09781ae68fb226e
James Holland <jholland@usgs.gov>
2012-12-31 15:06:09 -0700

    Added 1.01 as default value for all data and aggregate cells in the table. This allowed datatables to automatically determined datatypes.

--- station_health/js/summary.js
+++ station_health/js/summary.js
@@ -47,11 +47,12 @@ function buildGrid(){
             var $row = $('<tr id = "'+station+'"><td>'+mapGIDtoGName[mapSIDtoNID[station]]+'</td>'
                 +'<td id="l_'+station+'" class="ltd">'+mapSIDtoSName[station]+'</td>'
                 +'<td>,'+mapSIDtoGIDs[station]+',</td></tr>');
+            //Adding 1.01 causes datatables to automatically set the column types to numeric
             for( var i = 0; i<metricsSorted.length; i++){
-                $row.append('<td id="d_'+mapMNametoMID[metricsSorted[i]]+'_'+station+'" class="ltd"></td>');
+                $row.append('<td id="d_'+mapMNametoMID[metricsSorted[i]]+'_'+station+'" class="ltd">1.01</td>');
             }
             //Append aggregate cell here
-            $row.append('<td id="a_'+station+'"></td>');
+            $row.append('<td id="a_'+station+'">1.01</td>');
             $("#grid tbody").append($row);
         }
     }
