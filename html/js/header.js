31c2199e8edb2b3e8445d18e974f8aa9eb204a01
James Holland <jholland@usgs.gov>
2013-02-05 17:24:26 -0700

    Finished header. Added styling to dataq.css Added DateRange, refresh button. Actions are still broken, until remaining functionality is included.

--- html/js/header.js
+++ html/js/header.js
@@ -7,7 +7,6 @@ License: Public Domain
 
 function setupHeader(){
     var header = $("#header");
-    //Add return link
     if(pageType == "station"){
         var stationID = getQueryString("station");
         header.append(
@@ -17,10 +16,38 @@ function setupHeader(){
             window.location = "dataq.html?&sdate="+getStartDate("simple")+"&edate="+getEndDate("simple");
         });
 
-        header.append("<span class='dataqHeader'>"+mapGIDtoGName[mapSIDtoNID[stationID]]+"-"+mapSIDtoSName[stationID]+"</span>");
+        header.append(
+            "<span class='headerTitle'>"
+            +mapGIDtoGName[mapSIDtoNID[stationID]]
+            +"-"
+            +mapSIDtoSName[stationID]
+            +"</span>"
+        );
     }
     else if(pageType == "summary"){
-        header.append("<span class='dataqHeader'>Station Summary</span>");
+        header.append("<span class='headerTitle'>Station Summary</span>");
 
     }
+    //Adding span for dateRange now, but the dates and their controls will be added in the dateselection code.
+    var rightSide = $("<span class='right'></span>");
+    var dateSpan = $("<span id='headerDateRange'></span>");
+
+    dateSpan.append(
+        "<label for='dpStartDate'>  From</label>"+
+            "<input type='text' id='dpStartDate' name='dpStartDate' class='ddl'/>"
+    );
+    dateSpan.append(
+        "<label for='dpEndDate'>  To</label>"+
+            "<input type='text' id='dpEndDate' name='dpEndDate' class='ddl'/>"
+    );
+    
+    rightSide.append(dateSpan);
+    rightSide.append(
+        "<button type='button' id='btnRefresh' class='ui-state-default ui-corner-all'>Refresh</button>"
+    );
+    header.append(rightSide);
+    $("#btnRefresh").on("click",function(){
+        clearDataTable(dataGrid);
+        populateGrid(dataGrid);
+    });
 }
